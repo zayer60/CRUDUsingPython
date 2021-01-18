87f283698973d11e41c80611ec0676744d662413
zayer60 <75667757+zayer60@users.noreply.github.com>
2021-01-18 17:40:13 +0530

    file handling csv to db

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 
 class Database:
@@ -49,3 +50,31 @@ class Database:
         conn.commit()
         conn.close()
 
+    def insert_from_csv(self):
+        conn = sqlite3.connect("students.db")
+        with open('studentsdata.csv') as file:
+            contents = csv.DictReader(file, delimiter=',')
+            values = [(i['roll'], i['name'], i['dob'], i['gender']) for i in contents]
+            #values = []
+            #for row in contents:
+             #   value
+                # value = [row[0], row[1], row[2], row[3], row[4]]
+              #  values.append(row)
+
+        cur = conn.cursor()
+        cur.executemany("INSERT INTO student VALUES(NULL,?,?,?,?)", values)
+        conn.commit()
+        conn.close()
+
+    def show_from_csv(self):
+        conn = sqlite3.connect("students.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM student")
+        with open('studentsdata.csv', 'w') as file:
+            contents = csv.writer(file)
+            contents.writerow([i[0] for i in cur.description])
+            contents.writerows(cur)
+
+database=Database("students.db")
+database.insert_from_csv()
+print(database.view())
